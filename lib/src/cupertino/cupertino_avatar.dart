b305771e224bd4688ef3857e0bda668310d6d921
xiamo <xiaomo@xiaomo.info>
2019-10-29 23:22:47 +0900

    remove cache image

--- lib/src/cupertino/cupertino_avatar.dart
+++ lib/src/cupertino/cupertino_avatar.dart
@@ -1,19 +1,19 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/material.dart';
-
-class CAvatar extends StatelessWidget {
-  final String url;
-  final double width;
-  final double height;
-
-  CAvatar(this.url, {this.width, this.height});
-
-  @override
-  Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: url,
-      width: width,
-      height: height,
-    );
-  }
-}
+//import 'package:cached_network_image/cached_network_image.dart';
+//import 'package:flutter/material.dart';
+//
+//class CAvatar extends StatelessWidget {
+//  final String url;
+//  final double width;
+//  final double height;
+//
+//  CAvatar(this.url, {this.width, this.height});
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return CachedNetworkImage(
+//      imageUrl: url,
+//      width: width,
+//      height: height,
+//    );
+//  }
+//}
